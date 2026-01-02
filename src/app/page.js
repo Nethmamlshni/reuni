c616bd4632f1ea40c4f6a55667ab0e399d29054d
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2026-01-03 04:43:03 +0530

    feat: restructure home page layout with Navbar, Hero, FAQ, and Footer components

--- src/app/page.js
+++ src/app/page.js
@@ -1,31 +1,18 @@
 "use client";
 
-import { useEffect, useState } from "react";
+
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+import Hero from "@/src/features/home/components/Hero";
+import FAQ from "@/src/features/home/components/FAQ";
 
 export default function Home() {
-  const [message, setMessage] = useState("");
-
-  useEffect(() => {
-    fetch("/api/hello")
-      .then((res) => res.json())
-      .then((data) => {
-        setMessage(data.message);
-      })
-      .catch((err) => console.error(err));
-  }, []);
-
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold text-gray-800 dark:text-gray-200">
-          Welcome to My Backend App!
-        </h1>
-
-        {/* Display API message */}
-        <p className="mt-4 text-xl text-blue-600 dark:text-blue-400">
-          {message}
-        </p>
-      </div>
-    </div>
-  );
+    return (
+        <>
+            <Navbar/>
+            <Hero/>
+            <FAQ/>
+            <Footer/>
+        </>
+    );
 }
