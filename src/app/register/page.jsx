1741369297de435f5c47693779173e2b9ab775d4
Chamod Malindu <chamod10malindu@gmail.com>
2026-01-03 02:28:24 +0530

    fix: ShadCn error

---
+++ src/app/register/page.jsx
@@ -0,0 +1,10 @@
+import { Button } from "@/@/components/ui/button";
+
+
+export default function RegisterPage() {
+  return <div>
+    Register Page
+    <Button>Click Me</Button>
+    <Button></Button>
+    </div>;
+}
