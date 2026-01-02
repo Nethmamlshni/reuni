b953614f2c94c828d5b87790a604ac8427fc33e1
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2026-01-03 04:56:37 +0530

    feat: add Features component to home page layout

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,14 @@ import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import Hero from "@/src/features/home/components/Hero";
 import FAQ from "@/src/features/home/components/FAQ";
+import Features from "@/src/features/home/components/Features";
 
 export default function Home() {
     return (
         <>
             <Navbar/>
             <Hero/>
+            <Features/>
             <FAQ/>
             <Footer/>
         </>
