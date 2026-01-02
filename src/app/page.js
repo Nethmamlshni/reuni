dc8dd8a0d8f19fd7cce0353e9c55482ba1dedcf1
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2026-01-03 05:17:53 +0530

    feat: add About and CTA components to home page

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,8 @@ import Footer from "@/components/layout/Footer";
 import Hero from "@/src/features/home/components/Hero";
 import FAQ from "@/src/features/home/components/FAQ";
 import Features from "@/src/features/home/components/Features";
+import About from "@/src/features/home/components/About";
+import CTA from "@/src/features/home/components/CTA";
 
 export default function Home() {
     return (
@@ -14,7 +16,10 @@ export default function Home() {
             <Hero/>
             <Features/>
             <FAQ/>
+            <About/>
+            <CTA/>
             <Footer/>
+
         </>
     );
 }
